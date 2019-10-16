54e518d4c4aabbf2dc0185e643be02984110d0a8
Stephan Schiffels <schiffels@damp133250.w-open.shh.mpg.de>
2019-10-16 11:29:10 +0200

    removed password (and changed it)

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ncftpput -R -v -u stephansbo -p N6aHWWLU8usO -P 21 ftp.cluster026.hosting.ovh.net www _site/*
+ncftpput -R -v -u stephansbo -P 21 ftp.cluster026.hosting.ovh.net www _site/*
